4413e2305193a2be0bf00c85a1f2c29c62e65613
Tarjei Husøy <tarjei@roms.no>
2013-11-16 19:26:46 +0100

    Restyle movie search results.
    Also make sure we save the current search query in the URL to make it
    refresh safe. This is purely to make it faster to develop against,
    so this might be skipped in production if it slows things down.
    
    Some refactoring of the search.js as well.

--- www/javascripts/search.js
+++ www/javascripts/search.js
@@ -15,12 +15,17 @@ var pendingPostMessage;
 (function ($, marvin) {
     "use strict";
 
-    $('.movie-search').on('keyup', function () {
-        var searchQuery = $(this).val();
+    var $searchField = $('.movie-search');
+
+    $searchField.on('keyup', searchMovies);
+
+    function searchMovies() {
+        var searchQuery = $searchField.val();
+        history.replaceState(null, null, '?search=' + searchQuery);
         marvin.movies.search(searchQuery, function (response) {
             var renderedHtml = marvin.templates.movies(response);
             $('#search_results').html(renderedHtml);
-
+            scaleStreamCount();
             var search_results = {}; // we will save all results here, so we later reference it
 
             // Loop through all results and assign them in a dictionary
@@ -46,5 +51,33 @@ var pendingPostMessage;
             });
 
         });
-    });
+    }
+
+    function scaleStreamCount() {
+        var $elem = $('.movie-stream-count');
+        var fontSize = 65;
+        var ourText = $('span:visible:first', $elem);
+        var maxHeight = $elem.height();
+        var maxWidth = $elem.width();
+        var textHeight;
+        var textWidth;
+        do {
+            ourText.css('font-size', fontSize);
+            textHeight = ourText.height();
+            textWidth = ourText.width();
+            fontSize = fontSize - 1;
+        } while ((textHeight > maxHeight || textWidth > maxWidth) && fontSize > 3);
+    }
+
+    // Onload stuff
+    (function setSearchValFromUrl() {
+        var search = $.url().param('search');
+        if (search) {
+            $searchField.val(search);
+            searchMovies();
+        }
+    })();
+
+    $(window).resize(scaleStreamCount);
+
 })(jQuery, marvin);
